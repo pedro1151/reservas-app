fbc4831e5b860f71b86e29046bdd8112c63492ec
Pedro Ramos <pedroramosfine@gmail.com>
2026-02-27 18:54:00 -0300

    se setea ip gcp

--- app/src/main/java/com/optic/pramosreservasappz/core/Config.kt
+++ app/src/main/java/com/optic/pramosreservasappz/core/Config.kt
@@ -2,17 +2,17 @@ package com.optic.pramosreservasappz.core
 
 object  Config {
 
-    //const val URL_BASE = "http://136.113.44.38/"
-    const val URL_BASE = "http://10.0.2.2/"
-
+    const val URL_BASE = "http://136.113.44.38/"
+    //const val URL_BASE = "http://10.0.2.2/"
+/*
     const val BASE_URL = "http://10.0.2.2:8006/"  //reservas auth
     const val BASE_URL_TEAMS = "http://10.0.2.2:8008/"  //reservas
     const val BASE_URL_EXTERNAL_SERVICES = "http://10.0.2.2:8007/" //reservas external
     const val AUTH_PREFERENCE = "AUTH_PREF"
     const val AUTH_KEY = "AUTH_KEY"
+*/
 
 
- /*
 
     // IP DE GCP --REMOTO -PRODUCCION : 34.136.16.162  ( SIN PUERTO ya que internamente usa nginx puerto 80)
     const val BASE_URL = URL_BASE
@@ -21,7 +21,7 @@ object  Config {
     const val AUTH_PREFERENCE = "AUTH_PREF"
     const val AUTH_KEY = "AUTH_KEY"
 
-*/
+
     /*google auht */
     const val CLIENT_ID_GOOGLE ="433710092520-m7644hv1215dgfqr11ks4dseusl8rcgl.apps.googleusercontent.com"
 
